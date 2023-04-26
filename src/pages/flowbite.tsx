5b222027978bfba1ebd54312929239acccc0fa6c
qvtec <qvtec3@gmail.com>
2023-04-27 08:32:02 +0900

    ✨

--- src/pages/flowbite.tsx
+++ src/pages/flowbite.tsx
@@ -1,9 +1,25 @@
 import Accordion from "@/components/pages/Flowbite/Accordion"
 import Alerts from "@/components/pages/Flowbite/Alerts"
+import Carousel from "@/components/pages/Flowbite/Carousel"
 
 export default function Flowbite() {
   return (
     <div className="p-4">
+      <h1 className="text-xl font-bold">Flowbite</h1>
+      <div className="mt-3 grid grid-cols-3 gap-2">
+        <div className="block p-6 border border-gray-300 rounded">
+          <Accordion />
+        </div>
+        <div className="block p-6 border border-gray-300 rounded">
+          <Alerts />
+        </div>
+        <div className="block p-6 border border-gray-300 rounded">
+          <Carousel />
+        </div>
+        <div className="flex items-center justify-center rounded bg-gray-50 h-28">
+            <p className="text-2xl text-gray-400">+</p>
+        </div>
+      </div>
       <div className="grid grid-cols-3 gap-4 mb-4">
         <div className="flex items-center justify-center h-24 rounded bg-gray-50">
             <p className="text-2xl text-gray-400">+</p>
@@ -24,20 +40,6 @@ export default function Flowbite() {
         </div>
         <div className="flex items-center justify-center rounded bg-gray-50 h-28">
             <p className="text-2xl text-gray-400">+</p>
-        </div>
-        <div className="flex items-center justify-center rounded bg-gray-50 h-28">
-            <p className="text-2xl text-gray-400">+</p>
-        </div>
-        <div className="flex items-center justify-center rounded bg-gray-50 h-28">
-            <p className="text-2xl text-gray-400">+</p>
-        </div>
-      </div>
-      <div className="grid grid-cols-3 gap-2">
-        <div className="block p-6 border border-gray-300 rounded-lg">
-          <Accordion />
-        </div>
-        <div className="block p-6 border border-gray-300 rounded-lg">
-          <Alerts />
         </div>
         <div className="flex items-center justify-center rounded bg-gray-50 h-28">
             <p className="text-2xl text-gray-400">+</p>
